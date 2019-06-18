d033a8e1af5952e1d1ff07a27aa2aa3e1ae637df
WCIofQMandRA <48008292+WCIofQMandRA@users.noreply.github.com>
2019-06-18 14:06:22 +0800

    Update variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -13,6 +13,6 @@ vector<CalcTwoVar_t> CalcTwoVar;
 vector<SystemFunction_t> SystemFunction;
 vector<int> SystemFunctionParameterCount;
 set<string> Keywordlist={"INPUT","PRINT","IF","THEN","ELSE","END","WHILE",
-"WEND","DO","LOOP","UNTIL","AND","OR","XOR","MOD","SET","VAL","TRUE","FALSE",
+"WEND","DO","LOOP","UNTIL","AND","OR","XOR","MOD","SET","VAR","TRUE","FALSE",
 "DEF","RETURN","FEND","MAIN","SQR","ABS","SIN","COS","TAN","ARCSIN","ARCCOS",
 "ARCTAN","LN","LG","EXP","FLOOR","CEIL","ROUND","FACT","PI","MAX","MIN","ROOT"};//暂时把系统函数视为关键字
